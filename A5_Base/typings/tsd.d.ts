63db72c31cd016a2c30e94e66e1d932015d4fbba
Alexander Cignarella <acignarella3@gatech.edu>
2016-06-15 17:19:35 -0400

    A5_Base removed

--- A5_Base/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="chroma-js/chroma-js.d.ts" />
-/// <reference path="gl-matrix/gl-matrix.d.ts" />
-/// <reference path="stats/stats.d.ts" />
-/// <reference path="howlerjs/howler.d.ts" />
